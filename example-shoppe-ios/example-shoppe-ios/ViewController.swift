8c03bc9816363839a1199766cc51145da380a86d
harold <harold@fullstory.com>
2020-02-24 13:35:47 -0500

    Innitial Commit

---
+++ example-shoppe-ios/example-shoppe-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  example-shoppe-ios
+//
+//  Created by Harold Davis Jr. on 2/24/20.
+//  Copyright © 2020 Harold Davis Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ example-shoppe-ios/example-shoppe-iosTests/example_shoppe_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  example_shoppe_iosTests.swift
+//  example-shoppe-iosTests
+//
+//  Created by Harold Davis Jr. on 2/24/20.
+//  Copyright © 2020 Harold Davis Jr. All rights reserved.
+//
+
+import XCTest
+@testable import example_shoppe_ios
+
+class example_shoppe_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
