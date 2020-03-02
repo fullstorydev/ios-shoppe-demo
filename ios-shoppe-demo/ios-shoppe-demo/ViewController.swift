aa59b9ebfe6f488dc63da76f9e567855442a3c72
harold <harold@fullstory.com>
2020-03-02 09:05:45 -0500

    Initial Commit: - Built project - Changed "Copyright © 2020 Harold Davis Jr." to "Copyright © 2020 FullStory" - Removed "//  Created by Harold Davis Jr. on 3/2/20."and left "// "

---
+++ ios-shoppe-demo/ios-shoppe-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-shoppe-demo
+//
+// 
+//  Copyright © 2020 FullStory All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
