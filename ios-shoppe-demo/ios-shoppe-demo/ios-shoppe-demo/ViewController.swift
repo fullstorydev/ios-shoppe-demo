099193d87aef57d6c1a8a82585130e517ea67f04
Harold <60398138+HaroldFS@users.noreply.github.com>
2020-03-03 11:40:16 -0500

    Harolddavis/ch106726/firstcommit (#4)
    * Created and setup .gitignore
    
    * Built project
    Changed "Copyright © 2020 Harold Davis Jr." to "Copyright © 2020 FullStory"
    Removed "// Created by Harold Davis Jr. on 3/2/20."and left "// "

---
+++ ios-shoppe-demo/ios-shoppe-demo/ios-shoppe-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-shoppe-demo
+//
+//  Created on 3/3/20.
+//  Copyright © 2020 FullStory All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-shoppe-demo/ios-shoppe-demo/ios-shoppe-demoTests/ios_shoppe_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_shoppe_demoTests.swift
+//  ios-shoppe-demoTests
+//
+//  Created on 3/3/20.
+//  Copyright © 2020 FullStory All rights reserved.
+//
+
+import XCTest
+@testable import ios_shoppe_demo
+
+class ios_shoppe_demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
