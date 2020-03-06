e23a29039195b5460494ecb847317e805e65adf9
harold <harold@fullstory.com>
2020-03-06 08:29:30 -0800

    Changed name to "Product" Added a Unit property

--- ios-shoppe-demo/ios-shoppe-demo/ios-shoppe-demo/ShoppeItem.swift
+++ ios-shoppe-demo/ios-shoppe-demo/ios-shoppe-demo/ShoppeItem.swift
@@ -9,17 +9,19 @@
 import Foundation
 import UIKit
 
-class ShoppeItem {
+class Product {
     var title: String
     var description: String
     var price: String
     var imageName: String
     var image: UIImage = UIImage()
+    var unit: String
 
     init(_ dict: [String: Any]) {
         self.title = dict["title"] as? String ?? ""
         self.description = dict["description"] as? String ?? ""
         self.price = dict["price"] as? String ?? ""
         self.imageName = dict["image"] as? String ?? ""
+        self.unit = dict["unit"] as? String ?? ""
     }
 }
