34316e40a18c208f1415e2864217c5401dda9bae
harold <harold@fullstory.com>
2020-03-06 11:55:58 -0800

    - change name to product - fixed error guard statment

--- ios-shoppe-demo/ios-shoppe-demo/ios-shoppe-demo/APIService.swift
+++ ios-shoppe-demo/ios-shoppe-demo/ios-shoppe-demo/APIService.swift
@@ -12,7 +12,7 @@ class APIService {
     static var sharedInstance = APIService()
 
     var baseURL: String = "http://reactshoppe.s3-website-us-east-1.amazonaws.com/data/products.json"
-    var shopItems: [[String : Any]] = [] // TODO: Change this to be an array of class ShopItem
+    var products: [[String : Any]] = [] // TODO: Change this to be an array of class Product
 
     func getShoppeItem(completion: @escaping()->()) {
         guard let url = URL(string: baseURL) else { return }
@@ -21,13 +21,16 @@ class APIService {
 
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let data = data else { return }
-            guard error != nil else { return }
+
+            if let error = error {
+                print("\(error.localizedDescription)") // TODO: Add proper error handling
+            }
 
             do {
                 let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [Any]
 
                 for dict in json ?? [""] {
-                    self.shopItems.append(dict as? [String : Any] ?? [:]) // TODO: use class ShopItem
+                    self.products.append(dict as? [String : Any] ?? [:]) // TODO: use class Product
                 }
                 completion()
             }
