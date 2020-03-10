1719dea141d55035ee8acf74976f138035acb858
Harold <60398138+HaroldFS@users.noreply.github.com>
2020-03-10 09:55:28 -0700

    Harolddavis/ch106773/create an api service (#5)
    * Create an API Service
    
    - Create the singleton
    - Add main endpoint as a property
    - Setup header for method to get shop items
    
    * Allows Local Networking
    
    * Style consistancy.
    
    * - change name to product
    - fixed error guard statment
    
    * Changed the Directory name of where the project lives to swift
    
    * Adjusted getSho[[eItem Closure
    - Improved Try by adding a `catch` for errors
    
    * Added a config file (Preferences) to hold hardcoded Values
    Made baseURL a computed property that retrives the URL from the config file.
    
    * removed test
    
    * removed duplicate files
    renamed file from shopeeItem to product

---
+++ swift/ios-shoppe-demo/APIService.swift
@@ -0,0 +1,46 @@
+//
+//  APIService.swift
+//  ios-shoppe-demo
+//
+//  Created on 3/4/20.
+//  Copyright © 2020 FullStory All rights reserved.
+//
+
+import Foundation
+
+class APIService {
+    static var sharedInstance = APIService()
+
+    var baseURL: String {
+        guard let webserviceURL = getPlist(withName: "Preferences") else {
+            print("No web services URL found")
+            return ""
+        }
+        return webserviceURL[0]
+    }
+    
+    var products: [[String : Any]] = [] // TODO: Change this to be an array of class Product
+
+    func getShoppeItem(completion: @escaping()->()) {
+        guard let url = URL(string: baseURL) else { return }
+
+        let request = URLRequest(url: url)
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else { return }
+
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [Any]
+
+                for dict in json ?? [""] {
+                    self.products.append(dict as? [String : Any] ?? [:]) // TODO: use class Product
+                }
+                completion()
+            }
+            catch {
+                print("\(error.localizedDescription)") // TODO: Add proper error handling
+                completion()
+            }
+        }.resume()
+    }
+}
