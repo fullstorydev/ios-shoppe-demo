4c5bebdcc39e30e0fd2ece2c79071c3ba0be69d3
Harold <60398138+HaroldFS@users.noreply.github.com>
2020-03-23 08:55:17 -0700

    collection view cells and size (#11)

--- swift/ios-shoppe-demo/StoreViewController.swift
+++ swift/ios-shoppe-demo/StoreViewController.swift
@@ -8,15 +8,32 @@
 
 import UIKit
 
-class StoreViewController: UICollectionViewController {
+class StoreViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    var shopeeItems: [Product] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
 
-    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return shopeeItems.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 350, height: 200)
+    }
 }
-
