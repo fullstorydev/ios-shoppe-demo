2251c6e642f1dce7d58a3bf44b668c1234ddc72c
harold <harold@fullstory.com>
2020-03-23 05:22:52 -0700

    add local file to ios shoppe app
    - add local file to ios shoppe app so it can be used without a network call
    - moved products from being a global variable to being a local var in the closure
    - update closure to pass an array of products
    - add a new functions to grab to parse data from local js

---
+++ swift/ios-shoppe-demo/Resources/ShoppeItems.js
@@ -0,0 +1,114 @@
+[
+  {
+    "title": "Bananas",
+    "description": "The old reliable.",
+    "price": 2.99,
+    "image": "banans.jpg",
+    "unit": "lb."
+  },
+  {
+    "title": "Bluebs",
+    "description": "Extra organical and powered by blue.",
+    "price": 6.75,
+    "image": "bluebs.jpg",
+    "unit": "lb."
+  },
+  {
+    "title": "Carambola",
+    "description": "An all-star flavor.",
+    "price": 7.24,
+    "image": "carambola.jpg",
+    "unit": "lb."
+  },
+  {
+    "title": "Cherries",
+    "description": "Deeply red and passionate.",
+    "price": 6.99,
+    "image": "cherries.jpg",
+    "unit": "lb."
+  },
+  {
+    "title": "Cocktail Mix",
+    "description": "Virgin mix for adult use.",
+    "price": 15.00,
+    "image": "mixeddrink.jpg",
+    "unit": "gallon"
+  },
+  {
+    "title": "Dragon Fruit",
+    "description": "Kissed by fire, not spicy.",
+    "price": 8.43,
+    "image": "dragonfruit.jpg",
+    "unit": "ea."
+  },
+  {
+    "title": "Durian",
+    "description": "Smells worse than it looks.",
+    "price": 14.99,
+    "image": "durian.jpg",
+    "unit": "ea."
+  },
+  {
+    "title": "Grapes",
+    "description": "Use either for raisin or wine.",
+    "price": 7.24,
+    "image": "grapes.jpg",
+    "unit": "lb."
+  },
+  {
+    "title": "Green Beans",
+    "description": "Beans, beans... great for your heart.",
+    "price": 1.79,
+    "image": "beans.jpg",
+    "unit": "lb."
+  },
+  {
+    "title": "Mangocados",
+    "description": "Highly experimental new frewt.",
+    "price": 7.99,
+    "image": "mangocados.jpg",
+    "unit": "ea."
+  },
+  {
+    "title": "One Pear",
+    "description": "One lonely pear, eat while sobbing.",
+    "price": 4.99,
+    "image": "pear.jpg",
+    "unit": ""
+  },
+  {
+    "title": "Oranges de Florida",
+    "description": "Lose the sleeves, you're in Florida.",
+    "price": 4.99,
+    "image": "oranges.jpg",
+    "unit": "lb."
+  },
+  {
+    "title": "Peacharines",
+    "description": "This one's on us, Mother Nature.",
+    "price": 6.39,
+    "image": "peacherines.jpg",
+    "unit": "lb."
+  },
+  {
+    "title": "Peaches",
+    "description": "The pride and joy of Georgia.",
+    "price": 4.29,
+    "image": "peaches.jpg",
+    "unit": "lb."
+  },
+  {
+    "title": "Pears",
+    "description": "More than one pear, pears.",
+    "price": 9.22,
+    "image": "pears.jpg",
+    "unit": "lb."
+  },
+  {
+    "title": "Raspberries",
+    "description": "Next stop, Yumsville.",
+    "price": 5.99,
+    "image": "rasps.jpg",
+    "unit": "lb."
+  }
+]
