5677d6f7ab9c27fad9cf3a4b876db0defb2cba31
harold <harold@fullstory.com>
2020-03-30 06:32:48 -0700

    add-an-extension-on-uiview
    - added an extension to creat a curve card look on the ui of cells

---
+++ swift/ios-shoppe-demo/Extensions/EX+UIView.swift
@@ -0,0 +1,32 @@
+//
+//  EX+UIView.swift
+//  ios-shoppe-demo
+//
+//  Created by Harold Davis Jr. on 3/30/20.
+//  Copyright © 2020 Harold Davis Jr. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func curve() {
+        self.layer.cornerRadius = 20
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 3, height: 3)
+        self.layer.shadowOpacity = 0.2
+        self.layer.shadowRadius = 4.0
+    }
+
+    func smallCurve() {
+        self.layer.cornerRadius = 10
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 3, height: 3)
+        self.layer.shadowOpacity = 0.2
+        self.layer.shadowRadius = 5.0
+    }
+
+    func xtreamCurve() {
+        self.layer.cornerRadius = 40
+    }
+}
