0f4ed1f1b0f4304d0aa1cb941803e611590e18d1
harold <harold@fullstory.com>
2020-03-30 07:31:10 -0700

    create-order-object
    - create-order-object
    - removed more duplicate files

---
+++ swift/ios-shoppe-demo/Model/Order.swift
@@ -0,0 +1,14 @@
+//
+//  Order.swift
+//  ios-shoppe-demo
+//
+//  Created on 3/30/20.
+//  Copyright © 2020 FullStory All rights reserved.
+//
+
+import Foundation
+
+class Order {
+    var items: [Product] = []
+    var date: Date = Date()
+}
