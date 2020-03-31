15e7778fafebc691adac77b494bd3c92cc77e4a6
harold <harold@fullstory.com>
2020-03-31 09:28:10 -0700

    dding Order object

---
+++ swift/ios-shoppe-demo/Model/Order.swift
@@ -0,0 +1,14 @@
+//
+//  Order.swift
+//  ios-shoppe-demo
+//
+//  Created by Harold Davis Jr. on 3/31/20.
+//  Copyright © 2020 Harold Davis Jr. All rights reserved.
+//
+
+import Foundation
+
+class Order {
+    var items: [Product] = []
+    var date: Date = Date()
+}
