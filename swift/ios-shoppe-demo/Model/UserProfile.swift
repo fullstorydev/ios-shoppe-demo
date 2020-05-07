a3171e255c291283028c279511a4086686240a01
harold <harold@fullstory.com>
2020-05-07 05:54:49 -0700

    Personal header

--- swift/ios-shoppe-demo/Model/UserProfile.swift
+++ swift/ios-shoppe-demo/Model/UserProfile.swift
@@ -2,8 +2,8 @@
 //  UserProfile.swift
 //  ios-shoppe-demo
 //
-//  Created by Harold Davis Jr. on 4/30/20.
-//  Copyright © 2020 Harold Davis Jr. All rights reserved.
+//  Created on 4/30/20.
+//  Copyright © 2020 FullStory All rights reserved.
 //
 
 import Foundation
