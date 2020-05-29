a2d87776262fdcf49867685571651e28182f969e
Harold <60398138+HaroldFS@users.noreply.github.com>
2020-05-29 07:29:36 -0700

    Fs manager (#37)
    * test
    
    * Fixing header for FS manager
    
    * I think to 2 key conveniences here are:
    
    `enum LogLevel {
    case assert, error, warning, info, debug
    }
    
    enum FSStatus: String {
    case exclude = "fs-exclude"
    case mask = "fs-mask"
    case unmask = "fs-unmask"
    }`
    
    ...as well as the methods createFSEvent and log
    
    Maybe I will remove them from this singleton since there is no good reason yet for it to be this way. The original reason is it would have a currentCustomEvent to hold the status of where the user is in the app, it would have a boolean value for userConsentGiven, etc...
    
    For now I am abstracting the 4 items out of a class as they are something that can be copy/pasted into a given project.
    
    LogLevel
    
    FSStatus
    
    fsCreateEvent
    
    fsLog
    
    fsModify
    
    * FS Manager
    - changed fsModify -> fsModifyPrivacy
    - changed FSStatus -> PrivacySetting

---
+++ swift/ios-shoppe-demo/ServiceManagers/FullStoryManager.swift
@@ -0,0 +1,64 @@
+//
+//  FullStoryManager.swift
+//  ios-shoppe-demo
+//
+//  Created 5/7/20.
+//  Copyright © 2020 FullStory All rights reserved.
+//
+
+import Foundation
+import FullStory
+
+/**
+ FullStory Manager is a file full of functions and enums that can make the FullStory API easier to apply to areas of your project.
+    - create events
+    - logging and unmasking elements
+ */
+
+enum Event: String {
+    case browsing
+    case itemSelected
+    case viewCart
+    case addToCart
+    case checkout
+    case removeFromCart
+    case crash
+}
+
+enum LogLevel {
+    case assert, error, warning, info, debug
+}
+
+enum PrivacySetting: String {
+    case exclude = "fs-exclude"
+    case mask = "fs-mask"
+    case unmask = "fs-unmask"
+}
+
+
+func fsCreateEvent(event: Event, with dict: [String: Any]) {
+    FS.event(event.rawValue, properties: dict)
+}
+
+func fsLog(message: String, level: LogLevel = .info) {
+    var fsLogType: FSEventLogLevel
+
+    switch level {
+    case .assert:
+        fsLogType = FSLOG_ASSERT
+    case .error:
+        fsLogType = FSLOG_ERROR
+    case .warning:
+        fsLogType = FSLOG_WARNING
+    case .info:
+        fsLogType = FSLOG_INFO
+    case .debug:
+        fsLogType = FSLOG_DEBUG
+    }
+
+    FS.log(with: fsLogType, message: "\(Date())" + message)
+}
+
+func fsModifyPrivacy(setting: PrivacySetting, of view: UIView) {
+    FS.addClass(view, className: setting.rawValue)
+}
