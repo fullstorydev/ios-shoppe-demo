5430969c7e6080ae7f8003590ed3fc937437fbf0
harold <harold@fullstory.com>
2020-07-27 06:22:53 -0700

    Part 2 FS Manager file Documentation

--- swift/ios-shoppe-demo/ServiceManagers/FullStoryManager.swift
+++ swift/ios-shoppe-demo/ServiceManagers/FullStoryManager.swift
@@ -10,14 +10,15 @@ import Foundation
 import FullStory
 
 /*
- FullStory Manager is a file full of functions and enums that can make the FullStory API easier to apply to areas of your project.
+FullStory Manager provides common functions and artifacts that make it easier to integrate FullStory into your iOS app.
+    - Events
+    - LogLevel
     - Identifying a user
     - create events
-    - logging
-    - masking, unmasking, and excluding elements
+    - PrivacySetting: EXCLUDE, MASK, and UNMASK elements
  */
 
-/// We created an enum that represents the major events in the iOS app. (This could be different for you based on the flow of your app.)
+/// The Event enum represents major events in an iOS app. These may differ based on your particular app.
 enum Event: String {
     case browsing
     case itemSelected
@@ -32,7 +33,10 @@ enum LogLevel {
     case assert, error, warning, info, debug
 }
 
-/// For the hardcoded values that represent the privacy settings we have an example of more enum utilization here.
+/** For the hardcoded values that represent the privacy settings we have an example of more enum utilization here.
+
+ - For more info on how to use a  https://help.fullstory.com/hc/en-us/articles/360044349073-FullStory-Private-by-Default#exclude-fs-exclude
+*/
 enum PrivacySetting: String {
     case exclude = "fs-exclude"
     case mask = "fs-mask"
@@ -41,8 +45,8 @@ enum PrivacySetting: String {
 
 /**
  This is a convenience method that takes a case and a dictionary to title the event and provide properties.
-    - Parameter event: `browsing`, `itemSelected`, `viewCart`, `addToCart`, `checkout`, `removeFromCart`, and `crash` (This could be different for you based on the flow of your app.).
-    - Parameter dictionary: This should be a dictionary containing valuable info gathered at the time of the event.
+    - Parameter event: This is an Event type.
+    - Parameter dict: This should be a dictionary containing valuable info gathered at the time of the event.
  */
 func fsCreateEvent(event: Event, with dict: [String: Any]) {
     FS.event(event.rawValue, properties: dict)
@@ -52,7 +56,7 @@ func fsCreateEvent(event: Event, with dict: [String: Any]) {
   This method will allow you to pass a level as a parameter using dot notation with a message that will send logs to the dev tools on the FullStory app.
 
     - Parameter message: A text string that will be logged to the FullStory console.
-    - Parameter level: 'assert', 'info', 'warning', 'error', and 'debug'. (This is optional and will be set as info by defualt)
+    - Parameter level: This a LogLevel type.
  */
 func fsLog(message: String, level: LogLevel = .info) {
     var fsLogType: FSEventLogLevel
